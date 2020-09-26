c44e8dd36c65900fdc43a049331f29f397668e2a
Mehulll <mehul.aryan.2222@gmail.com>
2020-09-27 00:59:00 +0530

    Javascript tweet action handler

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -16,9 +16,13 @@ class TweetActionSerializer(serializers.Serializer):
             raise serializers.ValidationError("This is not a valid action for tweets")
         return value
 class TweetSerializer(serializers.ModelSerializer):
+    likes = serializers.SerializerMethodField(read_only = True)
     class Meta:
         model = Tweet
-        fields = ['content']
+        fields = ['id','content','likes']
+        
+    def get_likes(self,obj):
+        return obj.likes.count()
         
     def validate_content(self,value):
         if len(value) > MAX_TWEET_LENGTH:
