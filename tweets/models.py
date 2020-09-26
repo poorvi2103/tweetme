997ddc270cd490d0c40908fa98de355cb7ded424
Mehulll <mehul.aryan.2222@gmail.com>
2020-09-26 18:03:36 +0530

    Users & tweets

--- tweets/models.py
+++ tweets/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 import random
+from django.conf import settings
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
     #SQL
     #id = models.AutoField(primary_key = True)
+    user = models.ForeignKey(User , on_delete = models.CASCADE)
     content = models.TextField(blank = True , null = True)
     image = models.FileField(upload_to = 'images/' , blank = True , null = True)
     
