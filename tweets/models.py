2f9e29c8fff25a8dc6c2603f49c1d3044fd421f2
Mehulll <mehul.aryan.2222@gmail.com>
2020-09-27 17:01:15 +0530

    retweeting logic

--- tweets/models.py
+++ tweets/models.py
@@ -13,14 +13,15 @@ class TweetLike(models.Model):
 class Tweet(models.Model):
     #SQL
     #id = models.AutoField(primary_key = True)
+    parent = models.ForeignKey("self" , null = True , on_delete = models.SET_NULL)
     user = models.ForeignKey(User , on_delete = models.CASCADE)
     likes = models.ManyToManyField(User , related_name = 'tweet_user',blank = True,through = TweetLike)
     content = models.TextField(blank = True , null = True)
     image = models.FileField(upload_to = 'images/' , blank = True , null = True)
     timestamp = models.DateTimeField(auto_now_add = True)
     
-    def __str__(self):
-        return self.content
+    # def __str__(self):
+    #     return self.content
     
     class Meta:
         ordering = ['-id']
