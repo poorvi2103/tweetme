91e16e5ae0935ae08ecdfa94d30c0a594e4b3118
Mehulll <mehul.aryan.2222@gmail.com>
2020-09-28 10:19:05 +0530

    ajax lookup via xhr in react js

--- tweetme-web/src/App.js
+++ tweetme-web/src/App.js
@@ -1,14 +1,38 @@
-import React , {useEffect , useState} from 'react';
+import React , {useCallback, useEffect , useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function loadTweets(callback){
+    const xhr = new XMLHttpRequest() // xhr = Someclass()
+    const method = 'GET' // POST
+    const url = "http://127.0.0.1:8000/api/tweets"
+    const responseType = "json"
+    xhr.responseType = responseType
+    xhr.open(method,url)
+    xhr.onload = function(){
+        callback(xhr.response,xhr.status)
+    }
+    xhr.onerror = function (e) {
+      console.log(e)
+      callback({"message" : "The request was an error"},400)
+    }
+    xhr.send()
+}
+
 
 function App() {
-  const [tweets , setTweets] = useState([])
+  const [tweets,setTweets] = useState([])
   useEffect(() => {
-    // do my lookup
-    const tweetItems = [{"content":123},{"content":"Hello world"}]
-    setTweets(tweetItems)
+    const myCallback = (response,status) => {
+      if(status === 200){
+        setTweets(response)
+      }
+      else{
+        alert("There was an error")
+      }
+    }
+    loadTweets(myCallback)
+
   },[])
   return (
     <div className="App">
