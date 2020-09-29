b68b1f99125034da8db23208895f08d91a2d5771
Mehulll <mehul.aryan.2222@gmail.com>
2020-09-29 14:54:34 +0530

    create tweet in react

--- tweetme-web/src/tweets/components.js
+++ tweetme-web/src/tweets/components.js
@@ -1,6 +1,6 @@
 import React , {useEffect,useState} from 'react'
 
-import {loadTweets} from '../lookup'
+import {createTweet , loadTweets} from '../lookup'
 
 export function TweetComponent(props){
   const textAreaRef = React.createRef()
@@ -9,11 +9,16 @@ export function TweetComponent(props){
     event.preventDefault()
     const newVal = textAreaRef.current.value
     let tempNewTweets = [...newTweets]
-    tempNewTweets.unshift({
-      content : newVal,
-      likes : 0,
-      id : 12313
+    // change this to a server side call
+    createTweet(newVal , (response,status) => {
+      if(status === 201){
+        tempNewTweets.unshift(response)
+      }else{
+        console.log(response)
+        alert("An error occured please try again")
+      }
     })
+
     setNewTweets(tempNewTweets)
     textAreaRef.current.value = ''
   }
