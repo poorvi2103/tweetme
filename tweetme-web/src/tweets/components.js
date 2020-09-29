6765c3f9e1da2d9ce65e45d8b0f525af9b489aa0
Mehulll <mehul.aryan.2222@gmail.com>
2020-09-29 17:44:48 +0530

    rendering the retweet

--- tweetme-web/src/tweets/components.js
+++ tweetme-web/src/tweets/components.js
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from 'react'
 
-import {apiTweetCreate, apiTweetList} from './lookup'
+import {
+apiTweetAction,
+apiTweetCreate, 
+apiTweetList} from './lookup'
 
 export function TweetComponent(props) {
     const textAreaRef = React.createRef()
@@ -77,11 +80,11 @@ export function ActionBtn(props) {
             ? tweet.likes
             : 0
     )
-    const [userLike, setUserLike] = useState(
-        tweet.userLike === true
-            ? true
-            : false
-    )
+//  const [userLike, setUserLike] = useState(
+//         tweet.userLike === true
+//             ? true
+//             : false
+//     )
     const className = props.className
         ? props.className
         : 'btn btn-primary btn-sm'
@@ -89,19 +92,16 @@ export function ActionBtn(props) {
         ? action.display
         : 'Action'
 
+    const handleActionBackendEvent = (response , status) =>{
+        if(status === 200){
+            setLikes(response.likes)
+           // setUserLike(true)
+        }
+    }
     const handleClick = (event) => {
         event.preventDefault()
-        if (action.type === 'like') {
-            if (userLike === true) {
-                // perhaps i Unlike it?
-                setLikes(likes - 1)
-                setUserLike(false)
-            } else {
-                setLikes(likes + 1)
-                setUserLike(true)
-            }
-
-        }
+        apiTweetAction(tweet.id ,action.type,handleActionBackendEvent )
+        
     }
     const display = action.type === 'like'
         ? `${likes} ${actionDisplay}`
