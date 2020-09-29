604db4cd9547a4a9868608720854421c7a213dce
Mehulll <mehul.aryan.2222@gmail.com>
2020-09-29 12:10:38 +0530

    adjust the react render process

---
+++ tweetme-web/build/precache-manifest.477fefcafb7ad3081407d25db97e7b16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ed300da1e745b01bad5704b22f5bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04d8dd1c75f7be194243",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "321cc9389aae9cc0520d",
+    "url": "/static/js/2.10658f80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10658f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d8dd1c75f7be194243",
+    "url": "/static/js/main.4377b6cd.chunk.js"
+  },
+  {
+    "revision": "4541c01a22991827904f",
+    "url": "/static/js/runtime-main.94a179c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- tweetme-web/src/index.js
+++ tweetme-web/src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {TweetComponent} from './tweets';
 
-ReactDOM.render(
+
+const appEl = document.getElementById('root')
+if(appEl){
+  ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  appEl);
+}
+
+const tweetsEl = document.getElementById('tweetme')
+if(tweetsEl){
+   ReactDOM.render(
+  <React.StrictMode>
+    <TweetComponent />
+  </React.StrictMode>,
+  tweetsEl);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
